e721d6fbb34f38bd1ecd0b9e97d6801f526b716a
Alex Seifert <alex@alexseifert.com>
2026-06-12 17:06:28 +0200

    Move most of the build command logic to the core

--- bin/build_index.php
+++ bin/build_index.php
@@ -3,30 +3,6 @@
 
 declare(strict_types=1);
 
-/**
- * Build (or rebuild) the SQLite index from the filesystem.
- *
- * Usage:
- *   php bin/build_index.php          # silent
- *   php bin/build_index.php -v       # verbose
- *   php bin/build_index.php --verbose
- */
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use BlogCore\Builders\IndexBuilder;
-use Sandbox\Config\BlogConfig;
-
-$verbose = in_array('-v', $argv ?? [], true)
-        || in_array('--verbose', $argv ?? [], true);
-
-$config  = new BlogConfig();
-$builder = new IndexBuilder($config);
-
-try {
-    $builder->build($verbose);
-    echo "Done." . PHP_EOL;
-} catch (Throwable $e) {
-    fwrite(STDERR, "Error: " . $e->getMessage() . PHP_EOL);
-    exit(1);
-}
+BlogCore\Commands\BuildIndexCommand::main($argv);
---
+++ blog-core.config.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Sandbox\Config\BlogConfig;
+
+return new BlogConfig();
