534e60eb9ec8413e26f45659c8d425146da54a2a
Alex Seifert <alex@alexseifert.com>
2026-06-12 17:40:39 +0200

    Add the ability to have frontend assets to the blog core

---
+++ bin/publish_assets.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use BlogCore\Commands\PublishAssetsCommand;
+use Sandbox\Config\BlogConfig;
+
+PublishAssetsCommand::run(new BlogConfig(), $argv);
+echo "Done." . PHP_EOL;
