f3aa62f319a4dac228ebd9d05aa490db4399b5a3
Alex Seifert <eiskalteschatten@users.noreply.github.com>
2026-07-15 22:46:40 +0200

    Show comments on the single post page

--- views/pages/posts/single.php
+++ views/pages/posts/single.php
@@ -2,6 +2,7 @@
 /** @var array $post */
 /** @var array $categories */
 /** @var array $tags */
+/** @var array $comments */
 /** @var \BlogCore\Core\Config $config */
 $pageTitle = $post['title'];
 ?>
@@ -29,6 +30,29 @@ $pageTitle = $post['title'];
         <div class="tag-list">
             <?php foreach ($tags as $tag): ?>
                 <a class="tag" href="/tags/<?= htmlspecialchars($tag['slug']) ?>"><?= htmlspecialchars($tag['name']) ?></a>
+            <?php endforeach ?>
+        </div>
+    <?php endif ?>
+
+    <?php if (!empty($comments)): ?>
+        <hr>
+        <h2>Comments (<?= count($comments) ?>)</h2>
+        <div class="comments-list">
+            <?php foreach ($comments as $comment): ?>
+                <?php
+                $author = trim((string)($comment['author'] ?? '')) ?: 'Anonymous';
+                $dateRaw = (string)($comment['comment_date'] ?? $comment['date'] ?? '');
+                $content = (string)($comment['content'] ?? '');
+                ?>
+                <article class="comment-item">
+                    <p>
+                        <strong><?= htmlspecialchars($author) ?></strong>
+                        <?php if ($dateRaw !== ''): ?>
+                            <span> on <?= htmlspecialchars(date('F j, Y H:i', strtotime($dateRaw))) ?></span>
+                        <?php endif ?>
+                    </p>
+                    <div><?= nl2br(htmlspecialchars($content), false) ?></div>
+                </article>
             <?php endforeach ?>
         </div>
     <?php endif ?>
